00015a1c24be468e26eaa28e35663232b5b16a8e
chungnd <ndchungict@icloud.com>
2024-08-05 15:45:01 +0700

    add faker dependency and update registration with random email

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,7 +1,7 @@
 import myAccountPage from "../pages/myAccountPage";
+import {faker} from "@faker-js/faker";
 
 const regData =require( "../fixtures/registrationData.json");
-
 describe('My Account - Registration', () => {
 
     beforeEach(() => {
@@ -9,7 +9,9 @@ describe('My Account - Registration', () => {
     });
 
     it('1. Registration - Sign in', () => {
-        // myAccountPage.register("","");
+        let newEmail = faker.internet.email();
+        myAccountPage.register(newEmail,"ChungND@AutomationTest1991");
+        myAccountPage.verifyLoginSuccessful();
     });
 
     regData.forEach((data) => {
