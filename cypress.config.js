272c799f0031aee20e34735f075bc8da0eb74b44
AutomationTest <AutoND@Automation-MacMinis-Mac-mini.local>
2024-08-19 00:39:57 -0700

    report portal config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,27 @@
 const { defineConfig } = require("cypress");
+const registerReportPortalPlugin = require('@reportportal/agent-js-cypress/lib/plugin');
 
 module.exports = defineConfig({
+  reporter: '@reportportal/agent-js-cypress',
+  reporterOptions: {
+    apiKey: 'ChungND_au2FMtPAQ9iH3VA2cke0BdQraI3BILdxgRUeLopSM9QjrcF1F5Lkc6egJZ5cfhb_',
+    endpoint: 'https://demo.reportportal.io',
+    project: 'ndchungict_personal',
+    launch: 'My launch',
+    description: 'Your launch description',
+    // attributes: [
+    //   {
+    //     key: 'attributeKey',
+    //     value: 'attributeValue',
+    //   },
+    //   {
+    //     value: 'anotherAttributeValue',
+    //   },
+    // ],
+  },
   e2e: {
     setupNodeEvents(on, config) {
-
+      return registerReportPortalPlugin(on, config);
     },
     baseUrl: 'https://practice.automationtesting.in',
     // viewportWidth: 1366,
